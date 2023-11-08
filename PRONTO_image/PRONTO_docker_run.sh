57b00c8719329bc03d8d8a8af9694cf559be9e23
Xiaoli Zhang <81294502+xiaoliz0@users.noreply.github.com>
2023-11-08 13:34:11 +0100

    Update PRONTO_docker_run.sh

--- PRONTO_image/PRONTO_docker_run.sh
+++ PRONTO_image/PRONTO_docker_run.sh
@@ -3,5 +3,5 @@
 echo -n "´Please enter your local path for TSOPPI results:"
 read tsoppi_data
 pronto_dir=$(dirname "$PWD")
-tag="docker_pronto:v1"
+tag="pronto:v1"
 sudo docker run --rm -it -v $tsoppi_data:/pronto/tsoppi_data -v $pronto_dir/Config:/pronto/Config -v $pronto_dir/In:/pronto/In -v $pronto_dir/Out:/pronto/Out -v $pronto_dir/Script:/pronto/Script $tag
