8dea0a187d6bad077943cc6c5fdb52e01505d9c0
Xiaoli Zhang <81294502+xiaoliz0@users.noreply.github.com>
2023-11-08 13:17:06 +0100

    Add files via upload

---
+++ PRONTO_image/PRONTO_singularity_run.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+echo -n "´Please enter your local path for TSOPPI results:"
+read tsoppi_data
+pronto_dir=$(dirname "$PWD")
+singularity exec --no-home -B $tsoppi_data:/pronto/tsoppi_data -B $pronto_dir/Config:/pronto/Config -B $pronto_dir/In:/pronto/In -B $pronto_dir/Out:/pronto/Out -B $pronto_dir/Script:/pronto/Script $pronto_dir/PRONTO_image/PRONTO_singularity/PRONTO_v1_singularity_image.sif python /pronto/Script/PRONTO.py
